c68fef415830bdce96391592392c80eaa09073e9
CkHong <hck1205@daum.net>
2020-11-29 20:38:46 +0900

    updated initial webpack configuration

---
+++ src/pages/main/index.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+type Props = {};
+
+function Presenter({}: Props) {
+  useEffect(() => {}, []);
+
+  return <div>main</div>;
+}
+
+export default Presenter;
