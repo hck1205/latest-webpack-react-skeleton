40a90b83312c5ef8ce410c5576ba6169cd3809a6
CkHong <hck1205@daum.net>
2020-12-19 16:22:46 +0900

    added unstated-next for global store

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import RootStore from 'stores';
+
 import App from './pages/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <RootStore.Provider>
+    <App />
+  </RootStore.Provider>,
+  document.getElementById('root')
+);
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,11 +1,27 @@
 import React, { useEffect } from 'react';
 
+import rootStore, { RootStore } from 'stores';
+
 type Props = {};
 
 function Presenter({}: Props) {
+  const { userStore }: RootStore = rootStore.useContainer();
+  const { userName, setUserName } = userStore;
+
   useEffect(() => {}, []);
 
-  return <div>main</div>;
+  return (
+    <div>
+      {userName}
+      <button
+        onClick={() => {
+          setUserName('CkHong');
+        }}
+      >
+        Click To Change
+      </button>
+    </div>
+  );
 }
 
 export default Presenter;
